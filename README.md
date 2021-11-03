1357252843da704463154165b9f8738fcb5661d9
7farema <7farema@gmail.com>
2021-11-03 21:53:21 +0700

    work

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # studi-kasus-1-vanzeven
 studi-kasus-1-vanzeven created by GitHub Classroom
-
-coba2 adminlte
--- login_action.php
+++
@@ -1,9 +0,0 @@
-Welcome <?php echo $_POST["email"]; ?><br>
-Your pwd is: <?php echo $_POST["password"]; ?>
-
-<?php
-
-print_r($_POST);
-var_dump($_POST);
-
-?>
